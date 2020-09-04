7c9eb4ef2b3fb04c870066f2d0192ca99bd6f0ef
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2020-09-04 16:05:55 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
+++ app/src/main/java/com/github/tatsuyafujisaki/githubactionssample/MainActivity.kt
@@ -9,8 +9,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = "test"
-    
+    val aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = "test"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
